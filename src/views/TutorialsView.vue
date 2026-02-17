733acfe26aec3ea1627db3ed685ff06067f45391
vagranted <nik21.dance@gmail.com>
2026-02-17 23:25:27 +0700

    Исправил баг с неправильными роутами

--- src/views/TutorialsView.vue
+++ src/views/TutorialsView.vue
@@ -2,10 +2,10 @@
   <div class="container mt-4">
     <div class="card">
       <div class="card-header bg-primary text-white">
-        <h3>Правки</h3>
+        <h3>Здесь хранятся туториалы</h3>
       </div>
       <div class="card-body">
-        <p>Правки</p>
+        <p>Туториалы</p>
       </div>
     </div>
   </div>
